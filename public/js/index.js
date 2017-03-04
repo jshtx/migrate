772110a5fbb904af19ee85a142552a26fc285ba1
CamWren <wrnecmrn@gmail.com>
2017-03-04 10:37:04 -0600

    Moved Indeed API code to proper index.js file locate within public folder

--- public/js/index.js
+++ public/js/index.js
@@ -31,4 +31,41 @@ lock.on("authenticated", function(authResult) {
 });
 
 
+// INDEED API AJAX CALL
+var userIp = [];
+
+  $.getJSON("http://jsonip.com/?callback=?", function (data) {
+    userIp.push(data.ip);
+  });
+
+// https://www.indeed.com/jobs?as_and=software+developer&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=fulltime&st=&salary=50000&radius=25&l=Austin&fromage=any&limit=10&sort=&psf=advsrch
+
+  $("#submit").on("click", function(event) {
+
+      // var location =;
+      // var radius =;
+      // var siteType =;
+      // var jobType =;
+      // var limit =;
+      // var country=;
+      // var salary=;
+
+          event.preventDefault();
+
+          var queryURL = "http://api.indeed.com/ads/apisearch?publisher=6697079064168197&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=20&filter=&latlong=1&co=us&chnl=&userip="+ userIp[0] +"&useragent=MMozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36&v=2&format=json";
+      
+          $.ajax({
+                  url: queryURL,
+                  method: "GET"
+              })
+           
+              .done(function(response) {
+                  
+                  var results = JSON.stringify(response, null, 2);
+                  console.log(results);
+                  
+              });
+  });
+// END OF INDEED API AJAX CALL
+
 }
