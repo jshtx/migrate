4d76ef66f1d2de951da5149d4e550b8de7c45405
John Paul Wicklund <John Paul Wicklund>
2017-03-08 19:11:35 -0600

    updated with survey js

---
+++ db/citySurvey.js
@@ -0,0 +1,152 @@
+//start of code to log selection.
+var values = ["100","100","100","100","100","100","100","100","100","100"];
+
+//updates question 1 with dropdown selection
+var select = document.getElementById('question1');
+select.addEventListener('change', function() {
+
+  for (var i = 0; i < select.selectedOptions.length; i++) {
+    var option = select.selectedOptions[i];
+    values.splice(0, 1, option.value);
+  };
+  myBestMatch();
+});
+
+//updates question 2 with dropdown selection
+var select2 = document.getElementById('question2');
+select2.addEventListener('change', function() {
+
+  for (var i = 0; i < select2.selectedOptions.length; i++) {
+    var option = select2.selectedOptions[i];
+    values.splice(1, 1, option.value);
+  };
+  myBestMatch();
+});
+
+//updates question 3 with dropdown selection
+var select3 = document.getElementById('question3');
+select3.addEventListener('change', function() {
+
+  for (var i = 0; i < select3.selectedOptions.length; i++) {
+    var option = select3.selectedOptions[i];
+    values.splice(2, 1, option.value);
+  };
+  myBestMatch();
+});
+
+//updates question 4 with dropdown selection
+var select4 = document.getElementById('question4');
+select4.addEventListener('change', function() {
+
+  for (var i = 0; i < select4.selectedOptions.length; i++) {
+    var option = select4.selectedOptions[i];
+    values.splice(3, 1, option.value);
+  };
+  myBestMatch();
+});
+
+//updates question 5 with dropdown selection
+var select5 = document.getElementById('question5');
+select5.addEventListener('change', function() {
+
+  for (var i = 0; i < select5.selectedOptions.length; i++) {
+    var option = select5.selectedOptions[i];
+    values.splice(4, 1, option.value);
+  };
+  myBestMatch();
+});
+
+//updates question 6 with dropdown selection
+var select6 = document.getElementById('question6');
+select6.addEventListener('change', function() {
+
+  for (var i = 0; i < select6.selectedOptions.length; i++) {
+    var option = select6.selectedOptions[i];
+    values.splice(5, 1, option.value);
+  };
+  myBestMatch();
+});
+
+//updates question 7 with dropdown selection
+var select7 = document.getElementById('question7');
+select7.addEventListener('change', function() {
+
+  for (var i = 0; i < select7.selectedOptions.length; i++) {
+    var option = select7.selectedOptions[i];
+    values.splice(6, 1, option.value);
+  };
+  myBestMatch();
+});
+
+//updates question 8 with dropdown selection
+var select8 = document.getElementById('question8');
+select8.addEventListener('change', function() {
+
+  for (var i = 0; i < select8.selectedOptions.length; i++) {
+    var option = select8.selectedOptions[i];
+    values.splice(7, 1, option.value);
+  };
+  myBestMatch();
+});
+
+//updates question 9 with dropdown selection
+var select9 = document.getElementById('question9');
+select9.addEventListener('change', function() {
+
+  for (var i = 0; i < select9.selectedOptions.length; i++) {
+    var option = select9.selectedOptions[i];
+    values.splice(8, 1, option.value);
+  };
+  myBestMatch();
+});
+
+//updates question 10 with dropdown selection
+var select10 = document.getElementById('question10');
+select10.addEventListener('change', function() {
+
+  for (var i = 0; i < select10.selectedOptions.length; i++) {
+    var option = select10.selectedOptions[i];
+    values.splice(9, 1, option.value);
+  };
+  myBestMatch();
+});
+
+//function which finds the best city based on the survey results the user has entered.
+function myBestMatch() {
+  var bestMatch = {
+    city: "",
+    state: "",
+    cityDifference: 1000
+  };
+
+  var totalDifference = 0;
+
+  // Here we loop through all the city possibilities in the database.
+  for (var i = 0; i < cityScores.length; i++) {
+
+    // console.log(cityScores[i]);
+    totalDifference = 0;
+
+    // We calculate the difference between the scores and sum them into the totalDifference. If they have not selected a value yet the score defaults to 100 to take that item out of consideration.
+    for (var j = 0; j < values.length; j++) {
+      if (values[j] > 10) {
+        totalDifference = totalDifference + 100 }
+        else {
+        totalDifference = totalDifference + Math.abs(parseInt(values[j]) - parseInt(cityScores[i].scores[j]));
+      }
+    }
+    console.log(totalDifference);
+
+    // If the sum of differences is less then the differences of the current "best match"
+    if (totalDifference <= bestMatch.cityDifference) {
+
+      // Reset the bestMatch to be the new city.
+      bestMatch.city = cityScores[i].city;
+      bestMatch.state = cityScores[i].state;
+      bestMatch.cityDifference = totalDifference;
+    }
+  }
+  console.log(bestMatch);
+  console.log(values);
+};
+}
